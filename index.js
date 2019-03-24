7956163fc596f7e9ee4af0049838c3a3c48a1c6a
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-03-24 18:12:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,8 @@ fs.readdir("./commands/", (err, files) => {
 client.on("ready", async () => {
   console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
 
-  client.user.setActivity("tutorials because Grey is confused", {type: "WATCHING"});
+  client.user.setActivity("startup", {type: "WATCHING"});
 
-  //bot.user.setGame("on SourceCade!");
 });
 
 client.on("message", async message => {
