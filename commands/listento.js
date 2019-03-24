12b36a9f762b72692aedcba9e6445a5e2dc456f1
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-03-24 18:49:10 -0400

    Update listento.js

--- commands/listento.js
+++ commands/listento.js
@@ -1,14 +1,12 @@
-module.exports.help = {
-  name: "listento"
-}
-
 exports.run = (client, message, args) => {
         const watchmessage = args.join(" ");
-    if(command === "listeningto"){
     if(!message.member.roles.some(r=>["Commanding Officer", "HIGH COMMAND"].includes(r.name)) )
     return message.reply("Nice try.");
     
     client.user.setActivity(watchmessage, {type: 2});
-    }
-}
     
+}
+
+module.exports.help = {
+  name: "listento"
+}
