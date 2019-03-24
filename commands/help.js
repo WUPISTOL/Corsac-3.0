38af8602abe75c78f5a55173a78b3d1264952634
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-03-24 18:07:37 -0400

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+    let embed = new Discord.RichEmbed ()
+    .setColor('#FFEE00')
+    .setTitle('COMMAND LIST')
+    .setDescription('*More commands coming soon. Current commands are:*')
+    .addField('**roll**', 'Rolls dice. Number of sides varies from 6-100.')
+    .addField('**reload**', 'Reloads an outdated command.')
+    .addField('**ping**', 'Self explanatory. Tells you the bot\'s current ping.')
+    .setFooter('All commands have the prefix (c!)')
+    .setTimestamp()
+        message.channel.send(embed);
+}
+module.exports.help = {
+  name: "help"
+}
