8a499ea9276d39cc6d4a968c0f192312374d63da
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-03-25 14:12:04 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,3 +2,7 @@ exports.run = async (client, message, args) => {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! The latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms.`);
   }
+
+module.exports.help = {
+  name: "ping"
+}
