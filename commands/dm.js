1f0ea33dec370fb5eed5d46e3d49e3b68ccf61e5
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-03-25 11:30:11 -0400

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+
+    if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
+      return message.reply("Nice try.");
+    
+    let dmember = message.mentions.members.first();
+    if(!dmember)
+      return message.reply("Please mention a user to dm.");
+
+    let dmessage = args.slice(1).join(' ');
+    if(!dmessage) return message.reply("You must supply a message!");
+      dmember.send(`A member from ${message.author.guild.name} sent you a message: ${dms}`)
+}
+
+module.exports.help = {
+  name: "dm"
+}
