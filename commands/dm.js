42038490ff068081bd9b7931b8de1d0f8fc49f6f
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-03-25 18:07:57 -0400

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -4,14 +4,15 @@ exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
       return message.reply("Nice try.");
     
+    let dauthor = message.author;
     let dmember = message.mentions.members.first();
-    let guildname = dmember.guild.name
     if(!dmember)
       return message.reply("Please mention a user to dm.");
-
+    
     let dmessage = args.slice(1).join(' ');
     if(!dmessage) return message.reply("You must supply a message!");
     dmember.send(`${dmember} A member from ${dmember.guild.name} sent you a message: ${dmessage}`)
+    dauthor.send(`You've sent your message to ${dmember}.`)
 }
 
 module.exports.help = {
