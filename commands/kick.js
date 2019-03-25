d41444e44b41c7cbc3f5f8acdeb4953eed0b2318
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-03-25 11:23:36 -0400

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+    if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
+      return message.reply("Nice try.");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a user to kick.");
+    if(!member.kickable) 
+      return message.reply("I can't kick this user.");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided.";
+      member.send(`${member} You've been banned from: ${member.guild.name} because: ${reason}`)
+    member.kick(reason)
+      .catch(error => message.reply(`${message.author}, I couldn't kick because of : ${error}`));
+    message.reply(`${message.author.tag} kicked ${member.user.tag} because: ${reason}`);
+}
+
+module.exports.help = {
+  name: "kick"
+}
