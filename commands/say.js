753216580c7bad23009240f874a9495356558c77
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-01 11:36:32 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    if(!message.member.roles.some(r=>["Commanding Officer"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["Contributor"].includes(r.name)) )
         return message.reply("I'm not saying that.");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
