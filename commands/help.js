d2461d58e09f449e6b4d18aebbbaac8b18ffa2e6
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-05 11:53:24 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,6 +7,7 @@ exports.run = (client, message, args) => {
     .addField('**roll**', 'Rolls dice. Number of sides varies from 6-100.')
     .addField('**reload**', 'Reloads an outdated command.')
     .addField('**ping**', 'Self explanatory. Tells you the bot\'s current ping.')
+    .addField('**avatar**, 'Displays you or someone else\'s avatar. \(Example: c!avatar @user\)')
     .setFooter('All commands have the prefix (c!)')
     .setTimestamp()
         message.channel.send(embed);
