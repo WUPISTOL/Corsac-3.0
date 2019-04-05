20a8a2effe3662f5e9e1e148abf0dcb515073d49
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-05 16:35:04 -0400

    Update giverole.js

--- commands/giverole.js
+++ commands/giverole.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-  let rargs = args.slice(1).join(" ");
   let torole = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!torole) return message.reply("Couldn't find user.");
   if(!message.author.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to Manage Members.");
+  let rargs = args.slice(1).join(" ");
   let grole = message.guild.roles.find(`name`, rargs);
 
     if(!grole) return message.reply("There was an error with the role. Perhaps you spelled it incorrectly.");
