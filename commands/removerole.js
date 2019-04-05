4a486337e28f602a0dbd27ffb8e551cd9119d93e
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-05 12:33:18 -0400

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -1,2 +1,14 @@
-const Discord = require("discord.js")
-exports.run(client, message, args)
+exports.run = async (client, message, args) => {
+  let norole = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!norole) return message.reply("Couldn't find user.");
+  if(!message.author.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to Manage Members.");
+  let rrole = message.guild.roles.find(`name`, "${args}");
+
+    if(!rrole) return message.reply("There was an error with the role. Perhaps you spelled it incorrectly.");
+  norole.removeRole(rrole.id); 
+  message.channel.send(`<@${norole.id}>\'s role has been removed.`);
+}
+  
+module.exports.help = {
+  name: "removerole"
+}
