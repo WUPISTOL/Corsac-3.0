97b61cc2f0bab815715afd2495db9c8751870976
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-05 19:17:24 -0400

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -1,15 +1,19 @@
-exports.run = async (client, message, args) => {
-  let norole = message.mentions.members.first();
-  if(!norole) return message.reply("Couldn't find user.");
-  if(!message.author.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to Manage Members.");
-  let nargs = args.slice(1).join(" ");
-  let rrole = message.guild.roles.find(`name`, nargs);
+const Discord = require("discord.js");
 
-    if(!rrole) return message.reply("There was an error with the role. Perhaps you spelled it incorrectly.");
-  norole.removeRole(rrole.id); 
-  message.channel.send(`<@${norole.id}>\'s role has been removed.`);
+module.exports.run = async (client, message, args) => {
+  let rargs = args.slice(1).join(" ");
+  let grole = message.guild.roles.find(`name`, rargs);
+  let torole = message.mentions.members.first();
+  if(!torole) return message.reply("Couldn't find user.");
+  if(!message.author.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to Manage Members.");
+
+
+  if(!grole) return message.reply("There was an error with the role. Perhaps you spelled it incorrectly.");
+
+  await(torole.removeRole(grole.id));
+  message.reply(`<@${torole.id}> has been given the ${grole.id} role.`);
 }
-  
+
 module.exports.help = {
-  name: "removerole"
+  name: "giverole"
 }
