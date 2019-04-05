9087da69ddd0ea36dadaba9e2662b4d2c07a6e8d
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-05 16:35:40 -0400

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -2,7 +2,8 @@ exports.run = async (client, message, args) => {
   let norole = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!norole) return message.reply("Couldn't find user.");
   if(!message.author.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have permission to Manage Members.");
-  let rrole = message.guild.roles.find(`name`, "${args}");
+  let nargs = args.slice(1).join(" ");
+  let rrole = message.guild.roles.find(`name`, nargs);
 
     if(!rrole) return message.reply("There was an error with the role. Perhaps you spelled it incorrectly.");
   norole.removeRole(rrole.id); 
