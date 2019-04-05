d6c3ac77bd3e09285ff4127017e33d48279a01d0
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-05 12:02:39 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,11 +4,11 @@ exports.run = (client, message, args) => {
     .setColor('#FFEE00')
     .setTitle('COMMAND LIST')
     .setDescription('*More commands coming soon. Current commands are:*')
-    .addField('**roll**', 'Rolls dice. Number of sides varies from 6-100.')
+    .addField('**roll**', 'Rolls dice. Nothing too complicated.')
     .addField('**reload**', 'Reloads an outdated command.')
     .addField('**ping**', 'Self explanatory. Tells you the bot\'s current ping.')
     .addField('**avatar**, 'Displays you or someone else\'s avatar. \(Example: c!avatar @user\)')
-    .setFooter('All commands have the prefix (c!)')
+    .setFooter('All commands have the prefix \(c!\)')
     .setTimestamp()
         message.channel.send(embed);
 }
