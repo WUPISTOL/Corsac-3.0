b9bc1da0cbbfef6ec9a621b16f2dbd5b57f9c437
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-05 20:16:04 -0400

    Create adminhelp.js

---
+++ commands/adminhelp.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+    let embed = new Discord.RichEmbed ()
+    .setColor('#00FF99')
+    .setTitle('ADMIN COMMAND LIST')
+    .setDescription('*More commands coming soon. Current commands are:*')
+    .addField('**ban & kick**', 'Simple. Bans or kicks heretics.')
+    .addField('**giverole & removerole**', 'Gives or removes roles accordingly.')
+    .addField('**purge**', 'deletes up to 100 messages.')
+    .addField('**play\, listento\, watch\, & stream**', 'Changes my current status.')
+    .addField('**dm**', 'DMs a user with a message.')
+    .addField('~~giverole~~', '*Supposed to give someone a role. Doesn\'t work yet.*')
+    .setFooter('All commands have the prefix (c!).')
+    .setTimestamp()
+        message.channel.send(embed);
+}
+module.exports.help = {
+  name: "adminhelp"
+}
