8d06893b102b15ca48f50aee3921f5e848418355
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-11 12:10:28 -0400

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   const channel = client.channels.get("510559055165521921");
-  if (!channel) return console.error("The channel does not exist!");
+  if (!channel) return console.error("Couldn't find channel.");
   channel.join().then(connection => {
-    console.log("Successfully connected.");
+    console.log("Connected successfully to voice channel");
   }).catch(e => {
     console.error(e);
   });
