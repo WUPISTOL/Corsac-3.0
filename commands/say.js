a38efec0297af6dfa87262a5a645bdf0efc02fb4
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-04-16 09:15:53 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["Commanding Officer", "LVL. 100 GODFATHER"].includes(r.name)) )
-        return message.reply("I'm not saying that.");
+        return message.reply("err");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
