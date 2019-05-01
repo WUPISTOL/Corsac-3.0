03abf2939589cca65194b1a3b21fcecc62d1c70d
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-01 17:29:14 -0400

    Update rich.js

--- commands/rich.js
+++ commands/rich.js
@@ -1,7 +1,7 @@
 
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
+if(!message.member.roles.some(r=>["Commanding Officer"].includes(r.name)) )
       return message.reply("I'm not saying that.");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
