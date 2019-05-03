d87e56a64d029fd28cb68069fcf1c5ea534ae96a
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 17:02:03 -0400

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -6,7 +6,7 @@ let outcomesIndex = Math.round(Math.random() * outcomes.length);
 const e = await message.channel.send("*A holographic coin materializes before you, and falls.*");
 setTimeout(() => {
     e.edit(`*It lands on ${outcomes[outcomesIndex]}.*`);
-}, 800);
+}, 1250);
 }
 
 module.exports.help = {
