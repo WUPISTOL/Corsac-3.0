31e3c3ba25532d9eb3a9e512f085453ecbbcbdad
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 15:57:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const config = require("./config.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client({disableEveryone: true});
+const cooldown = new Set();
 client.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
@@ -34,32 +35,23 @@ client.on("message", async message => {
   if(message.channel.type === "dm") return;
     
   let prefix = config.prefix;
+  
+    if(message.content.indexOf(prefix) !== 0) return;
+  
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
+  if (cooldown.has(message.author.id))
+  return message.reply("You're sending messages way too fast!");
+
+cooldown.add(message.author.id);
+setTimeout(() => {
+  cooldown.delete(message.author.id);
+}, 500);
                                 
   let commandfile = client.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(client, message, args);
-    
-    const blargs = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const pcommand = blargs.shift().toLowerCase();
-
-    if(message.content.indexOf(prefix) !== 0) return;
-    if(!cmd) return message.channel.send("Please use an actual command.")
-  
-        if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
-            return message.reply("Nice try.");
-
-        const numvar = parseInt(args[0], 10);
-    if(pcommand === "purge") {
-        if(!numvar || numvar < 2 || numvar > 100)
-            return message.reply("Please choose a number between 2 and 100 to purge.");
-    
-        const fetched = await message.channel.fetchMessages({limit: numvar});
-        message.channel.bulkDelete(fetched)
-            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
-    
+   if(!cmd) return message.channel.send("...?");
 });
 
 
