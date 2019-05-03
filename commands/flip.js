e7275f9ac9e4b43f8101b592b70be220bd93abc5
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 16:38:14 -0400

    Create flip.js

---
+++ commands/flip.js
@@ -0,0 +1,11 @@
+
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+let outcomes = ["Heads", "Tails"];
+let outcomesIndex = Math.round(Math.random() * outcomes.length);
+message.channel.send(outcomes[outcomesIndex]);
+}
+
+module.exports.help = {
+  name: "flip"
+}
