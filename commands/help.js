935f97d12d76c48c0dcfd53e0da261cc2ee7c8fa
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 17:07:59 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,6 +10,7 @@ exports.run = (client, message, args) => {
     .addField('**reload**', 'Reloads an outdated command.')
     .addField('**ping**', 'Self explanatory. Tells you the bot\'s current ping.')
     .addField('**avatar**', 'Displays your or somebody else\'s avatar. \(Example: c!avatar @user\)')
+    .addField('**id**', 'Grabs someone\'s user ID. Not sure why you\'d use it, though. \(Example: c!id @user\)')
     .setFooter('All commands have the prefix \(c!\). For Admin commands, use c!adminhelp.')
     .setTimestamp()
         message.channel.send(embed);
