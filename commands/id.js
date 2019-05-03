55d5af224ceca9281ea1b7f3a3393b4866f1b000
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 12:30:58 -0400

    Update id.js

--- commands/id.js
+++ commands/id.js
@@ -1,6 +1,11 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+      var imember = message.mentions.users.first();
+    if(!imember){
     message.channel.send(`${message.author.id}`);
+    } else {
+    message.channel.send(`${imember.id}`);
+    }
 }
 
 module.exports.help = {
