3645871beaae6d589a856a072cf7ee366112ebbf
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 16:58:49 -0400

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -3,7 +3,10 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 let outcomes = ["Heads", "Tails"];
 let outcomesIndex = Math.round(Math.random() * outcomes.length);
-message.channel.send(`${outcomes[outcomesIndex]}`);
+const e = await message.channel.send("*A holographic coin materializes before you, and falls.*");
+setTimeout(() => {
+    e.edit(`*It lands on ${outcomes[outcomesIndex]}.*`);
+}, 500);
 }
 
 module.exports.help = {
