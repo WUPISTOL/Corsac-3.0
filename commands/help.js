576db283cc4b77e700e89bca3a5ac80859f348ec
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 17:06:22 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,7 @@ exports.run = (client, message, args) => {
     .setColor('#FFEE00')
     .setTitle('COMMAND LIST')
     .setDescription('*More commands coming soon. Current commands are:*')
+    .addField('**foo**', 'Very simple command to check if the bot is working.')
     .addField('**roll**', 'Rolls dice. Nothing too complicated.')
     .addField('**flip**', 'Flips a coin.')
     .addField('**reload**', 'Reloads an outdated command.')
