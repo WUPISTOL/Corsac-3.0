f189e525777f30e2d14c039b5029adf51d5e0e6b
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-03 17:24:56 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,11 +2,10 @@ exports.run = async (message, client, args) => {
 
         if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
             return message.reply("Nice try.");
-        //Get the delete count as a number.
+        
         const numvar = parseInt(args[0], 10);
     
-        if(!numvar || numvar < 2 || numvar > 100)
-            return message.reply("Please choose a number between 2 and 100 to purge.");
+        if(!numvar || numvar < 2 || numvar > 100) return message.reply("Choose a number between 2 and 100 to purge.");
     
         const fetched = await message.channel.fetchMessages({limit: numvar});
         message.channel.bulkDelete(fetched)
