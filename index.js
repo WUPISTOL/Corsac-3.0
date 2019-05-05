e8da7f227e9afc355984d000ced10c5bc8c77b76
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-05 16:07:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,11 +59,10 @@ setTimeout(() => {
     if(message.content.indexOf(prefix) !== 0) return;
     if(!cmd) return message.channel.send("Please use an actual command.")
 
-        if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
-            return message.reply("Nice try.");
-
         const numvar = parseInt(args[0], 10);
     if(pcommand === "purge") {
+        if(!message.member.roles.some(r=>["HIGH COMMAND"].includes(r.name)) )
+            return message.reply("Nice try.");
 
         if(!numvar || numvar < 2 || numvar > 100)
             return message.reply("Please choose a number between 2 and 100 to purge.");
