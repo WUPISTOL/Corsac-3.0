0e688e9466fe452dcb85ba6c2b6b02b15adba1a7
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-04 20:11:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,6 @@ setTimeout(() => {
         if(!numvar || numvar < 2 || numvar > 100)
             return message.reply("Please choose a number between 2 and 100 to purge.");
                 
-        message.delete().catch(O_o=>{});
         const fetched = await message.channel.fetchMessages({limit: numvar});
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
