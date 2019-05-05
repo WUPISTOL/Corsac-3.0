982ae3945dd8203739a13f6bf3128489a294c3d1
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-05 16:19:09 -0400

    Update destroy.js

--- commands/destroy.js
+++ commands/destroy.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+      //      if (message.author.id !== 'id goes here') return message.reply("Nice try."); try this later
 if(!message.member.roles.some(r=>["bot-dev"].includes(r.name)) )
       return message.reply("Nice try.");
     client.destroy();
