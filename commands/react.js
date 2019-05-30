2aa6b1ab3794877509e139da752e0b2bedd55853
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-30 18:14:19 -0400

    Update react.js

--- commands/react.js
+++ commands/react.js
@@ -1,7 +1,7 @@
 
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-    message.react('434485279726764033');
+    message.react('583773264401137696');
 }
 
 module.exports.help = {
