dfa7b11a45203ad10a7d0554b24fac3e762ecebc
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-30 12:14:38 -0400

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 
-    if (!message.member.hasPermission("ADMINISTRATOR"))
-      return message.reply("Nice try.");
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Nice try.");
     
     let dauthor = message.author;
     let dmember = message.mentions.members.first();
