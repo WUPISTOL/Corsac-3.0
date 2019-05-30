651d54664787087c8fad97968e805d0c7aca0511
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-30 17:31:54 -0400

    Update emoji.js

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-  message.channel.send("test: <:chiefthonk:571072527741485068>");
+  message.channel.send("test: <a:KirbyJam:434485279726764033>");
 
   }
 
