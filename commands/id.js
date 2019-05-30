b8ab863b0f1c9dc06770cb1ba851f3b3563823ac
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-05-30 12:07:54 -0400

    Update id.js

--- commands/id.js
+++ commands/id.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-      var imember = message.mentions.users.first();
+    var imember = message.mentions.users.first();
+    var authors = message.author;
     if(!imember){
     message.delete().catch(O_o=>{}); 
-    message.author.send(`${message.author.id}`);
+    authors.send(`${message.author.id}`);
     } else {
     message.delete().catch(O_o=>{}); 
     imember.send(`${imember.id}`);
