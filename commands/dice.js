2f61ff0112454ba7a3c0473c1531bef4e0664ee9
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-10 14:43:57 -0400

    Create dice.js

---
+++ commands/dice.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+  const messageWords = message.content.split(' ');
+  const rollMod = messageWords.slice(2).join(' ');
+    if (messageWords.length === 1) {
+      return message.reply("Please specify the die and number of dice you want to roll. Modifiers go after the die. *\(Example: c!roll 1d20 +5\)*");
+    }
+
+    let sides = messageWords[1]; // !roll 20
+    let rolls = 1;
+          if(sides > 100) return message.reply("The maximum number of sides is 100.");
+    if (!isNaN(messageWords[1][0] / 1) && messageWords[1].includes('d')) {
+      // !roll 4d20
+      rolls = messageWords[1].split('d')[0] / 1;
+      sides = messageWords[1].split('d')[1];
+      if(sides > 100) return message.reply("The maximum number of sides is 100.");
+      if(rolls > 23) return message.reply("We don't have that many dice!");
+    } else if (messageWords[1][0] == 'd') {
+      // !roll d20
+      sides = sides.slice(1);
+      if(sides > 100) return message.reply("The maximum number of sides is 100.");
+    }
+    sides = sides / 1; // convert to number
+    if (isNaN(sides) || isNaN(rolls)) {
+      return;
+    }
+     let embed = new Discord.RichEmbed ()
+    .setColor('#FFAA00')
+    .setTitle('Die Roller')
+    .setFooter('All commands have the prefix (c!).')
+    .setTimestamp()
+    if (rolls > 1) {
+      const rollResults = [];
+      for (let i = 0; i < rolls; i++) {
+        rollResults.push(Math.floor(Math.random()*sides)+1);
+      }
+      const sum = rollResults.reduce((a,b) => a + b);
+      let dicesum = sum + (rollMod / 1)
+
+    .setDescription(`**Total: ${dicesum}**`)
+    .addField(`*${messageWords[1]} result*`, `You rolled: ${rollResults.toString()} | Modifier: ${rollMod}`)
+     message.channel.send(embed);
+    } else {
+      if(!messageWords[1].includes('d')) {
+      let roll = Math.floor(Math.random() * sides) + 1
+      let diesum = roll + (rollMod / 1)
+    .setDescription(`**Total: ${diesum}**`)
+    .addField(`*d${messageWords[1]} result*`, `You rolled a: ${roll} | Modifier: ${rollMod}`)
+    message.channel.send(embed);
+      } else {
+      let roll = Math.floor(Math.random() * sides) + 1
+      let diesum = roll + (rollMod / 1)
+    .setDescription(`**Total: ${diesum}**`)
+    .addField(`*${messageWords[1]} result*`, `You rolled a: ${roll} | Modifier: ${rollMod}`)
+    message.channel.send(embed);
+    }
+ };
+
+module.exports.help = {
+  name: "dice"
+}
