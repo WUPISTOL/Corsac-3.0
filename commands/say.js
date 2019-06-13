d725baa646b5ce055fd7aec544032e9a6ff432ec
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-13 16:09:05 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    //arctic
 if (message.author.id !== '178242199924899840') return message.reply("I'm not saying that.");
+if (message.author.id !== '395345841453400075') return message.reply("I'm not saying that.");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
