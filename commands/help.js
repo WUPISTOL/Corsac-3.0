8d617c1e3e79de8b806082af3a530519ac9312dd
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-21 16:49:44 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,8 @@ exports.run = (client, message, args) => {
     .addField('**ping**', 'Tells you the bot\'s current ping.')
     .addField('**avatar**', 'Displays your or somebody else\'s avatar. \(Example: c!avatar @user\)')
     .addField('**id**', 'Grabs someone\'s user ID. Not sure why you\'d use it, though.')
+    .addField('**creatorinfo**', 'Tells you a little about the bot\'s creator.')
+    .addField('**link**', 'Simply gives you the link to invite this bot.')
     .setFooter('All commands have the prefix \(c!\). For Admin commands, use c!adminhelp.')
     .setTimestamp()
         message.channel.send(embed);
