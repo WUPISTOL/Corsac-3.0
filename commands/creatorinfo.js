1519e7362e2fbbcdaa5215cfed2cf0d901b16830
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-21 15:53:13 -0400

    Update creatorinfo.js

--- commands/creatorinfo.js
+++ commands/creatorinfo.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   .setTitle('Creator Info')
   .setDescription('~~Why did I make this my hobby~~')
   .addField('Well, you probably know that the creator of this bot is Jari.', 'You know, the guy that likes birbs?')
-  .setFooter(`I was bored and created this bot on ${client.user.createdTimestamp()}`
+  .setFooter(`I was bored and created this bot on ${client.user.createdTimestamp()}`)
   .setTimestamp()
   message.channel.send(embed);
 }
