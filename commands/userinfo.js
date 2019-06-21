20f0f849940ed59c5fb6837430031ea4c30869be
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-21 19:53:03 -0400

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -5,12 +5,18 @@ exports.run = (client, message, args) => {
   let embed = new Discord.RichEmbed()
   .setThumbnail(message.author.avatarURL)
   .setColor('#9900CC')
-    message.channel.send(embed)
+  .setDescription(`${message.author} joined ${message.author.guild} at ${message.author.guild.joinedAt}`)
+  .addField('**User ID**', `${message.author.id}`)
+  .addField('**Account Creation Date**', `${message.author} joined Discord on ${message.author.createdAt}`)
+    message.channel.send(embed);
     } else {
       let uembed = new Discord.RichEmbed()
   .setThumbnail(amember.avatarURL)
-  .setColor('#275BF0')
-    message.channel.send(uembed)
+  .setColor('#9900CC')
+  .setDescription(`${amember} joined ${amember.guild} at ${amember.guild.joinedAt}`)
+  .addField('**User ID**', `${amember.id}`)
+  .addField('**Account Creation Date**', `${amember} joined Discord on ${amember.createdAt}`)
+    message.channel.send(uembed);
     }
 }
     module.exports.help = {
