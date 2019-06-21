d48fc1630d93d13061b2975dafee91cadb5b57f9
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-21 16:56:12 -0400

    Update creatorinfo.js

--- commands/creatorinfo.js
+++ commands/creatorinfo.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
   .setColor('##FFEE00')
   .setTitle('Creator Info')
   .setDescription('~~Why did I make this my hobby~~')
-  .addField('Well, you probably know that the creator of this bot is **Jari/WUPISTOL**.', 'You know, the guy that likes birbs?')
+  .addField('Well, you probably know that the creator of this bot is **Jari/WUPISTOL**.', 'You know, the guy that likes birbs? Well, one day he was bored as all hell and decided to make a bot. That bot is now Corsac. End of story.')
   .setFooter(`Sorry for being oddly specific but I was bored and created this bot version on ${client.user.createdAt}`)
   message.channel.send(embed);
 }
