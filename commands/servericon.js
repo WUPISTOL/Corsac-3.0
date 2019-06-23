ecb5030f8f7fcd8533d8c33b40edab2777d5c9fd
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-23 15:44:59 -0400

    Update servericon.js

--- commands/servericon.js
+++ commands/servericon.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
   var guildicon = message.guild.iconURL;
   let embed = new Discord.RichEmbed()
   .setImage(guildicon)
-  .setColor('#275BF0')
+  .setColor('#3300FF')
     message.channel.send(embed)
 }
     module.exports.help = {
