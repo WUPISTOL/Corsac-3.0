668195d4f2a3e24c843d2c70ec815031ea81483a
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-06-23 21:42:38 -0400

    Create random.js

---
+++ commands/random.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+  let users = message.guild.members
+  let rando = Math.floor(Math.random() * users.length) + 1
+  message.channel.send(`${users[rando]}`);
+}
+
+module.exports.help = {
+  name: "say"
+}
