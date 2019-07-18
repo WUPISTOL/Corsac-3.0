3ad09180144cb775671187d6ccf6cf019ad79429
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-07-18 13:19:21 -0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -26,7 +26,7 @@ exports.run = (client, message, args) => {
     if (isNaN(sides) || isNaN(rolls)) {
         return;
     }
-    if(sides = 20) {
+    if(sides === 20) {
     colour === '#FFAA00';
     } else {
     colour === '#C2C2C2';
