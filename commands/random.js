23a26990bf9190eb923f6e6a8829775923e53eca
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-07-23 09:20:56 -0400

    Update random.js

--- commands/random.js
+++ commands/random.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
   let users = message.guild.members;
-  let rando = (Math.floor(Math.random() * users.length) + 1);
+  let rando = (Math.floor(Math.random() * users.length));
   message.channel.send(`${users[rando]}`);
 }
 
