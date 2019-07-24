40973087e4bcd18d4644f499399a6d71a75b345e
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-07-24 08:29:36 -0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -2,10 +2,11 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     const messageWords = message.content.split(' ');
     const rollMod = messageWords.slice(2).join(' ');
-    if(isNaN(rollMod) || !rollMod) {
-        rollMod == 0
+    if (isNaN(rollMod) || !rollMod) {
+        rollMod = 0
     }
     var colour = '#FFAA00';
+    var description = `**${message.author.username}\'s ${messageWords[1]}**`;
     if (messageWords.length === 1) {
         return message.reply("Please specify the die and number of dice you want to roll. Modifiers go after the die. *\(Example: c!roll 1d20 +5\)*");
     }
@@ -13,7 +14,7 @@ exports.run = (client, message, args) => {
     let sides = messageWords[1];
     let rolls = 1;
     if (sides > 100) return message.reply("The maximum number of sides is 100.")
-        
+
     if (!isNaN(messageWords[1][0] / 1) && messageWords[1].includes('d')) {
         // !roll 4d20
         rolls = messageWords[1].split('d')[0] / 1;
@@ -29,11 +30,15 @@ exports.run = (client, message, args) => {
     if (isNaN(sides) || isNaN(rolls)) {
         return;
     }
-    if(messageWords[1].includes('20')) {
-    colour = '#FFAA00';
-    } else if (sides === 12){
+
+    if (!messageWords[1].includes('d')) {
+        description = `**${message.author.username}\'s ${rolls}d${messageWords[1]}**`
+    }
+    if (messageWords[1].includes('20')) {
+        colour = '#FFAA00';
+    } else if (sides === 12) {
         colour = '#000000';
-    } else if (sides === 10){
+    } else if (sides === 10) {
         colour = '#003CFF';
     } else if (sides === 8) {
         colour = '#AA00FF';
@@ -52,9 +57,9 @@ exports.run = (client, message, args) => {
         }
         const sum = rollResults.reduce((a, b) => a + b);
         let dicesum = sum + (rollMod / 1)
-            if(rollMod === 0)  {
-                rollMod = '0';
-            }
+        if (rollMod === 0) {
+            rollMod = '0';
+        }
         let embed = new Discord.RichEmbed()
             .setColor(colour)
             .setDescription('*Dice Tower*')
@@ -63,34 +68,22 @@ exports.run = (client, message, args) => {
             .setTimestamp()
         message.channel.send(embed);
     } else {
-        if (!messageWords[1].includes('d')) {
-            let roll = Math.floor(Math.random() * sides) + 1
-            let diesum = roll + (rollMod / 1)
-            if(rollMod === 0)  {
-                rollMod = '0';
-            }
-            let embed = new Discord.RichEmbed()
-                .setColor(colour)
-                .setDescription(`**${message.author.username}\'s ${rolls}d${messageWords[1]}**`)
-                .addField(`Result: **__${diesum}__**`, `[${roll}] + (${rollMod}) = ${diesum}`)
-                .setTimestamp()
-            message.channel.send(embed);
-        } else {
-            let roll = Math.floor(Math.random() * sides) + 1
-            let diesum = roll + (rollMod / 1)
-            if(rollMod === 0)  {
-                rollMod = '0';
-            }
-            let embed = new Discord.RichEmbed()
-                .setColor(colour)
-                .setDescription(`**${message.author.username}\'s ${messageWords[1]}**`)
-                .addField(`Result: **__${diesum}__**`, `[${roll}] + (${rollMod}) = ${diesum}`)
-                .setTimestamp()
-            message.channel.send(embed);
+        let roll = Math.floor(Math.random() * sides) + 1
+        let diesum = roll + (rollMod / 1)
+        if (rollMod === 0) {
+            rollMod = '0';
         }
+        let embed = new Discord.RichEmbed()
+            .setColor(colour)
+            .setDescription(description)
+            .addField(`Result: **__${diesum}__**`, `[${roll}] + (${rollMod}) = ${diesum}`)
+            .setTimestamp()
+        message.channel.send(embed);
     }
+}
 };
 
 module.exports.help = {
     name: "roll"
+name: "roll"
 }
