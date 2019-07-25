fc59be709a79d101cd88766ea16fdd53856848e5
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-07-25 09:01:29 -0400

    Update isadmin.js

--- commands/isadmin.js
+++ commands/isadmin.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     let Amember = message.mentions.users.first();
     if (!Amember.hasPermission("ADMINISTRATOR")) {
-    message.reply("This user is not an admin.");
-      } else {
-      message.reply("This user is an admin.");
-      }
-}
+        message.reply("This user is not an admin.")
+    } else {
+        message.reply("This user is an admin.")
+    }
+};
 
 module.exports.help = {
   name: "isadmin"
