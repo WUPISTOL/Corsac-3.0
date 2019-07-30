ab2d0b3b3581ff694e00ede90bd55fc2576d66b7
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-07-30 09:26:23 -0400

    Update rando.js

--- commands/rando.js
+++ commands/rando.js
@@ -3,7 +3,9 @@ exports.run = (client, message, args) => {
     let people = Array.from(message.member.guild.members);
     let count = message.member.guild.memberCount;
     let countIndex = (Math.round(Math.random() * count) - 1);
-    message.channel.send(`${people[countIndex]}`);
+    let victim = people[countIndex];
+    //"victim" bc they're gonna get pinged lma0
+    message.channel.send(`${victim.username}`);
 }
 
 module.exports.help = {
