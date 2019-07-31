73dc789976a90ea90e1ea5b151b281778a588b82
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-07-31 08:45:24 -0400

    Update rando.js

--- commands/rando.js
+++ commands/rando.js
@@ -1,11 +1,15 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+        return message.reply("rando machine broke :clown:");
     let people = Array.from(message.member.guild.members);
     let count = message.member.guild.memberCount;
     let countIndex = (Math.round(Math.random() * count) - 1);
     let victim = people[countIndex];
-    //"victim" bc they're gonna get pinged lma0
-    message.channel.send(`${victim}`);
+    //"victim" because they're gonna get pinged until I fix this
+    console.log(Object.getOwnPropertyNames(victim));
+    //need to get just the username without pinging and without the user id
+    //currently returns the id and then pings, like 384905734890,@user#0000
 }
 
 module.exports.help = {
