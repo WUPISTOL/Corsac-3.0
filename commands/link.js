4b6f8db7b99d21171a2b318b5611846d76cab976
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-09-01 14:55:26 -0400

    Update link.js

--- commands/link.js
+++ commands/link.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 let embed = new Discord.RichEmbed()
     .setColor('#00ED24')
-    .setDescription(`('[Here you are.](https://discordapp.com/oauth2/authorize?client_id=548982802951962678&scope=bot&permissions=8)')
+    .setDescription('[Here you are.](https://discordapp.com/oauth2/authorize?client_id=548982802951962678&scope=bot&permissions=8)')
 
   message.channel.send(embed);
 }
