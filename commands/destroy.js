1cf25458b3d553226a7664ef51659fd7c9964764
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-09-07 17:57:09 -0400

    Update destroy.js

--- commands/destroy.js
+++ commands/destroy.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
       if (message.author.id !== '395345841453400075') return message.reply("Nice try.");
       client.destroy();
 }
-
+//more useful than the update command.
 module.exports.help = {
   name: "destroy"
 }
