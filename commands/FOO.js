e2460e64e2466fac619f5ab06599f71019d405af
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-09-07 17:56:46 -0400

    Update FOO.js

--- commands/FOO.js
+++ commands/FOO.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     message.channel.send("FOO");
 }
-
+//someone would not stop bothering me about c!foo responding "bar", and so now there's c!FOO
 module.exports.help = {
   name: "FOO"
 }
