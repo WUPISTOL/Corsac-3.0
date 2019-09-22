e8e72b294a164bf7f191a94bd214802c202ef046
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-09-21 22:34:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -70,6 +70,17 @@ setTimeout(() => {
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
+      if(pcommand === "p") {
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+    return message.reply("Nice try.");
+
+        if(!numvar || numvar < 2 || numvar > 100)
+            return message.reply("Please choose a number between 2 and 100 to purge.");
+                
+        const fetched = await message.channel.fetchMessages({limit: numvar});
+        message.channel.bulkDelete(fetched)
+            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
 });
 //kirbyjam, it works
 client.login(process.env.BOT_TOKEN);
