ed754e7ac807ea397f8bb6fdcfe281a4f98cf84a
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-09-23 14:07:54 -0400

    Update testpurge.js

--- commands/testpurge.js
+++ commands/testpurge.js
@@ -1,18 +1,20 @@
 const Discord = require("discord.js");
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 if (!message.member.hasPermission("ADMINISTRATOR"))
     return message.reply("Nice try.");
     
-    const messageWords = message.content.split(' ');
-    const rollMod = messageWords.slice(2).join(' ');
-    
-    if(!numvar || numvar < 2 || numvar > 100)
+    const messageString = message.content.split(' ');
+    const Num = messageString.slice(1).join(' ');
+    if(!isNaN(Num)) {
+    if(!Num || Num < 2 || Num > 100)
     return message.reply("Please choose a number between 2 and 100 to purge.");
     
-    const fetched = await message.channel.fetchMessages({limit: numvar});
+    const fetched = await message.channel.fetchMessages({limit: Num});
     message.channel.bulkDelete(fetched)
     .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-            }
+    } else return;
+}
+
 module.exports.help = {
   name: "testpurge"
 }
