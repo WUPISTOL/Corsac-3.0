12f6d532563b5bed0b88616548c336db08456ec6
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-09-27 11:06:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -51,36 +51,10 @@ setTimeout(() => {
   let commandfile = client.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(client, message, args);
    if(!cmd) return message.channel.send("...?");
-  
-    const blargs = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const pcommand = blargs.shift().toLowerCase();
 
     if(message.content.indexOf(prefix) !== 0) return;
     if(!cmd) return message.channel.send("Please use an actual command.")
 
-        const numvar = parseInt(args[0], 10);
-    if(pcommand === "purge") {
-    if (!message.member.hasPermission("ADMINISTRATOR"))
-    return message.reply("Nice try.");
-
-        if(!numvar || numvar < 2 || numvar > 100)
-            return message.reply("Please choose a number between 2 and 100 to purge.");
-                
-        const fetched = await message.channel.fetchMessages({limit: numvar});
-        message.channel.bulkDelete(fetched)
-            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
-      if(pcommand === "p") {
-    if (!message.member.hasPermission("ADMINISTRATOR"))
-    return message.reply("Nice try.");
-
-        if(!numvar || numvar < 2 || numvar > 100)
-            return message.reply("Please choose a number between 2 and 100 to purge.");
-                
-        const fetched = await message.channel.fetchMessages({limit: numvar});
-        message.channel.bulkDelete(fetched)
-            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
 });
 //kirbyjam, it works
 client.login(process.env.BOT_TOKEN);
