83ee3bf2417e703feeaac3208cfaeee932cba7da
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-09-27 13:17:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 client.on("ready", async () => {
   console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
   var testo = "This is a test".split(' ');
-  console.log(`${testo[2]}`);
+  console.log(`${testo[3]}`);
 });
 process.on("unhandledRejection", console.error);
 
