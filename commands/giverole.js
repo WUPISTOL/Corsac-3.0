c8b3981b383f682a87b9586039cac330f5e23002
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-06 09:22:45 -0500

    Update giverole.js

--- commands/giverole.js
+++ commands/giverole.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
   let role = args.join(" ").slice(22);
   if(!role) return message.channel.send("You must provide a role to give.");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("that role doesn't exist.");
+  if(!gRole) return message.channel.send("That role doesn't exist.");
 
   if(rMember.roles.has(gRole.id)) return message.channel.send("This user already has that role.");
   rMember.addRole(gRole.id);
