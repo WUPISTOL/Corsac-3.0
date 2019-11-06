a2933bb773df4074d8b6df3819f6da97aef6cfe6
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-05 20:42:54 -0500

    Create grole.js

---
+++ commands/grole.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+      return message.reply("Nice try.");
+    
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.reply("Couldn't find user.");
+  let role = args.join(" ").slice(22);
+  if(!role) return message.channel.send("You must provide a role to give.");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.reply("that role doesn't exist.");
+
+  if(rMember.roles.has(gRole.id)) return message.channel.send("This user already has that role.");
+  rMember.addRole(gRole.id);
+    const e = await message.channel.send(":ok_hand:");
+setTimeout(() => {
+    e.delete();
+}, 2000);
+}
+
+module.exports.help = {
+  name: "grole"
+}
