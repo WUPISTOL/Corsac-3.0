17e972712c6df6d6e008676db99eb90a78f29bab
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-05 20:46:10 -0500

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
   let role = args.join(" ").slice(22);
   if(!role) return message.reply("You must provide a role to give.");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.channel.send("That role doesn't exist.");
+  if(!gRole) return message.reply("that role doesn't exist.");
 
   if(!rMember.roles.has(gRole.id)) return message.reply("This user does not have that role.");
   rMember.removeRole(gRole.id);
