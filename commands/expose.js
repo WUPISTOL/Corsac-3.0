f80e392bfccc320864599cc700f83b10788b9ce4
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-05 21:35:57 -0500

    Create expose.js

---
+++ commands/expose.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+var snipes = require("./snipe.json"); // file containing snipes
+let chn = `${message.channel.id}`;
+var snipechannel = snipes[chn]; // to call an specific deleted message I guess
+
+if (snipechannel[0] === "No snipes") {
+  message.channel.send("What? There are no deleted messages atm");
+} else {
+  const embed = {
+    "color": 5608903,
+    "footer": {
+      "text": `Sniped by: ${message.author.tag}`
+    },
+    "fields": [{
+      "name": `${snipechannel[1]} said...`,
+      "value": `${snipechannel[0]}`
+    }]
+  };
+  await message.channel.send({
+    embed
+  });
+  snipechannel[0] = "No snipes";
+
+  var fileName = './snipe.json';
+  var file = require(fileName);
+
+  fs.writeFile(fileName, JSON.stringify(file, null, 2), function(error) {
+    if (error) {
+      return console.log('oops');
+    }
+  });
+}
+}
+
+module.exports.help = {
+  name: "expose"
+}
