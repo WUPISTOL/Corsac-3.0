4d154b8a113c76c5d643455fd2212631bb296ad0
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-06 12:04:35 -0500

    Update giverole.js

--- commands/giverole.js
+++ commands/giverole.js
@@ -1,4 +1,4 @@
-   const Discord = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
@@ -6,20 +6,8 @@ module.exports.run = async (bot, message, args) => {
       return message.reply("Nice try.");
     
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember){
+  if(!rMember) return message.reply("mention a user to give a role.");
   let role = args.join(" ").slice(22);
-  if(!role) return message.channel.send("You must provide a role to give.");
-  let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.channel.send("That role doesn't exist.");
-
-  if(message.author.roles.has(gRole.id)) return message.channel.send("You already have that role.");
-  message.author.addRole(gRole.id);
-    const e = await message.channel.send(":ok_hand:");
-setTimeout(() => {
-    e.delete();
-}, 2000);
-  } else {
-       let role = args.join(" ").slice(22);
   if(!role) return message.channel.send("You must provide a role to give.");
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.channel.send("That role doesn't exist.");
@@ -30,7 +18,6 @@ setTimeout(() => {
 setTimeout(() => {
     e.delete();
 }, 2000);
-  }
 }
 
 module.exports.help = {
