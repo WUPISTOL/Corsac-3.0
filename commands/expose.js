529d8149f04ff99e6c5b410d57debbadd1b63680
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-12 11:38:44 -0500

    Update expose.js

--- commands/expose.js
+++ commands/expose.js
@@ -1,6 +1,14 @@
 const Discord = require("discord.js");
 const fs = require("fs");
+const cooldown = new Set();
 exports.run = async (client, message, args) => {
+  if (cooldown.has(message.author.id))
+        return message.reply("You can only expose once a minute!");
+
+    cooldown.add(message.author.id);
+    setTimeout(() => {
+        cooldown.delete(message.author.id);
+    }, 60000);
 var snipes = JSON.parse(fs.readFileSync("./snipe.json", "utf8")); // file containing snipes
 let chn = `${message.channel.id}`;
 var snipechannel = snipes[chn]; // to call an specific deleted message I guess
