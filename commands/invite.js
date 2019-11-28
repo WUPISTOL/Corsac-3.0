b4950407610f18df192d9d203b93a4dcd819a00f
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-28 14:50:52 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,10 +1,10 @@
-const Discord = require("discord.js");
+  const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-        let link = client.generateInvite(['ADMINISTRATOR'])
-        let embed = new Discord.RichEmbed()
-        .setColor('#00ED24')
-        .setDescription(`[Here you are.](${link})`)
-        message.channel.send(embed);
+let embed = new Discord.RichEmbed()
+    .setColor('#00ED24')
+    .setDescription('[Here you are.](https://discordapp.com/oauth2/authorize?client_id=548982802951962678&scope=bot&permissions=8)')
+
+  message.channel.send(embed);
 }
 module.exports.help = {
   name: "invite"
