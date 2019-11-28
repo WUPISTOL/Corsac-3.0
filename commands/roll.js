29b8493c976d2adced19b46e92196cccfb523f55
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-11-28 15:51:52 -0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
     const messageWords = message.content.split(' ');
     var rollMod = messageWords[2];
-    var reason = messageWords[3];
+    var reason = messageWords.slice(3).join(' ');
     console.log(rollMod);
     console.log(reason);
     
