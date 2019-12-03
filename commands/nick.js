7d3f7d5d59b12d8f50ec0b407acffb548c3d201a
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-12-03 13:11:32 -0500

    Create nick.js

---
+++ commands/nick.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+      return message.reply("Nice try.");
+      let member = message.mentions.members.first();
+      let reason = args.slice(1).join(' ');
+    if(!member) {
+    if(!reason) return "You must provide something to give yourself a nickname.";
+    message.author.setNickname(reason);
+      .catch(error => message.channel.send(`Error: ${error}`));
+      } else {
+    if(!reason) return "You must provide something to give them a nickname.";
+    member.setNickname(reason);
+      .catch(error => message.channel.send(`Error: ${error}`));
+      }
+    const e = await message.channel.send(":ok_hand:");
+    setTimeout(() => {
+    e.delete();
+}, 2000);
+
+}
+
+module.exports.help = {
+  name: "nick"
+}
