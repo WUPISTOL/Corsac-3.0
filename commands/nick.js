4d71019f644a5a8f6f79065ba4c0c775bccfbd49
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-12-04 08:57:58 -0500

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -1,22 +1,22 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-  //  if (!message.member.hasPermission("ADMINISTRATOR"))
-  //    return message.reply("Nice try.");
-  //    let member = message.mentions.members.first();
-  //    let reason = args.slice(1).join(' ');
-  //  if(!member) {
-  //  if(!reason) return "You must provide something to give yourself a nickname.";
-  //  message.author.setNickname(reason);
-  //    .catch(error => message.channel.send(`Error: ${error}`));
-  //    } else {
-  //  if(!reason) return "You must provide something to give them a nickname.";
-  //  member.setNickname(reason);
-  //    .catch(error => message.channel.send(`Error: ${error}`));
-  //    }
-  //  const e = await message.channel.send(":ok_hand:");
-  //  setTimeout(() => {
-  //  e.delete();
-//}, 2000);
+    if (!message.member.hasPermission("ADMINISTRATOR"))
+      return message.reply("Nice try.");
+      let member = message.mentions.members.first();
+      let reason = args.slice(1).join(' ');
+    if(!member) {
+    if(!reason) return "You must provide something to give yourself a nickname.";
+    message.author.setNickname(reason);
+      .catch(error => message.channel.send(`Error: ${error}`));
+      } else {
+    if(!reason) return "You must provide something to give them a nickname.";
+    member.setNickname(reason);
+      .catch(error => message.channel.send(`Error: ${error}`));
+      }
+    const e = await message.channel.send(":ok_hand:");
+    setTimeout(() => {
+    e.delete();
+}, 2000);
 
 }
 
