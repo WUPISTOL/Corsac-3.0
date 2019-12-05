8a141caf46cb2b26ce906827598956de386cb1d0
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-12-05 10:41:04 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -11,10 +11,11 @@ exports.run = async (client, message, args) => {
         //     message.channel.bulkDelete(fetched)
         var amount = 0
         for (var i = 0; i < Num; i++) {
-            channel.fetchMessages({ limit: 1 }).then(messages => {
+            message.channel.fetchMessages({ limit: 1 }).then(messages => {
             let lastMessage = messages.first();
             lastMessage.delete().catch(O_o => {});
             amount++;
+            }
         }
         message.channel.send(`Deleted ${amount} messages.`);  
     } else return;
