878852d1e23000fd329b970b9ff53ef48c453863
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2019-12-05 10:37:12 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,20 +1,23 @@
-const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("MANAGE_MESSAGES"))
-    return message.reply("You don't have the necessary permissions.");
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+        return message.reply("You don't have the necessary permissions.");
     const messageString = message.content.split(' ');
     const Num = messageString.slice(1).join(' ');
-    if(!isNaN(Num)) {
-    if(!Num || Num < 2 || Num > 100)
-    return message.reply("Please choose a number between 2 and 100 to purge.");
-    
-    const fetched = await message.channel.fetchMessages({limit: Num});
-    message.delete().catch(O_o=>{}); 
-    message.channel.bulkDelete(fetched)
-    .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    if (!isNaN(Num)) {
+        if (!Num || Num < 2 || Num > 100)
+            return message.reply("Please choose a number between 2 and 100 to purge.");
+
+        //     const fetched = await message.channel.fetchMessages({limit: Num});
+        //     message.channel.bulkDelete(fetched)
+        var amount = 0
+        for (var i = 0; i < Num; i++) {
+            message.channel.lastMessage.delete().catch(O_o => {});
+            amount++;
+        }
+        message.channel.send(`Deleted ${amount} messages.`);  
     } else return;
 }
 
 module.exports.help = {
-  name: "purge"
+    name: "purge"
 }
