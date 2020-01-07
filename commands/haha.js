6a31b2f121ae15a322a3c1e942422f9d644b91d1
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-01-07 18:20:44 -0500

    Create haha.js

---
+++ commands/haha.js
@@ -0,0 +1,8 @@
+exports.run = async (client, message, args) => {
+    message.channel.send("<:haha:664246257404870656>");
+    message.delete().catch(O_o=>{}); 
+  }
+
+module.exports.help = {
+  name: "haha"
+}
