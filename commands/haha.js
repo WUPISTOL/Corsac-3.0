681925a5697589cc28807cbced18cfed7c260472
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-01-07 19:16:59 -0500

    Update haha.js

--- commands/haha.js
+++ commands/haha.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-    message.channel.send("<:haha:664246257404870656>");
+    message.channel.send("<:haha:664261063532871697>");
     message.delete().catch(O_o=>{}); 
   }
 
