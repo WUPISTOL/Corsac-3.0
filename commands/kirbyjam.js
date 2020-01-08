2e3295e8d9c1db7d2580154e9a67aa136e2b7ffb
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-01-07 19:09:41 -0500

    Update kirbyjam.js

--- commands/kirbyjam.js
+++ commands/kirbyjam.js
@@ -1,6 +1,7 @@
 exports.run = async (client, message, args) => {
   let roll = Math.floor(Math.random() * 100) + 1;
-  if(roll === 1) {
+  if(roll === 99) {
+    message.delete().catch(O_o=>{}); 
     message.channel.send("<:haha:664246257404870656>")
   } else {
   message.channel.send("Poyo!");
