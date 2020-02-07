9d641fdd28ee4a9270a2ad28eeecc7803b121cba
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-02-07 11:21:13 -0500

    Delete haha.js

--- commands/haha.js
+++
@@ -1,8 +0,0 @@
-exports.run = async (client, message, args) => {
-    message.channel.send("<:haha:664261063532871697>");
-    message.delete().catch(O_o=>{}); 
-  }
-
-module.exports.help = {
-  name: "haha"
-}
