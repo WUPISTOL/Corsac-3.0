7b6579e17c800b96648b2034553203f3987a6a36
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-02-14 13:34:10 -0500

    Update p.js

--- commands/p.js
+++ commands/p.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
-if (!message.member.hasPermission("ADMINISTRATOR"))
-    return message.reply("Nice try.");
-    
+if (!message.member.hasPermission("MANAGE_MESSAGES") && (message.author.id !== '395345841453400075'))
+    return message.reply("You don't have the necessary permissions.");
     const messageString = message.content.split(' ');
     const Num = messageString.slice(1).join(' ');
     if(!isNaN(Num)) {
@@ -10,6 +9,7 @@ if (!message.member.hasPermission("ADMINISTRATOR"))
     return message.reply("Please choose a number between 2 and 100 to purge.");
     
     const fetched = await message.channel.fetchMessages({limit: Num});
+    message.delete().catch(O_o=>{}); 
     message.channel.bulkDelete(fetched)
     .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     } else return;
