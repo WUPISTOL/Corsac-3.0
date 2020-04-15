ce34f07b85bad56553bb39a68793a06b8b282702
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-04-14 20:10:34 -0400

    Update destroy.js

--- commands/destroy.js
+++ commands/destroy.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
-      if (message.author.id !== '395345841453400075') return message.reply("Nice try.");
+      if (message.author.id !== '395345841453400075') return;
       client.destroy();
 }
 //more useful than the update command.
