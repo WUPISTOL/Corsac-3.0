df344c1f7f50e84a3ac9bcfc3f437debe4040ae6
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-05-01 11:19:29 -0400

    Update expose.js

--- commands/expose.js
+++ commands/expose.js
@@ -1,3 +1,4 @@
+  
 const Discord = require("discord.js");
 const fs = require("fs");
 const cooldown = new Set();
@@ -8,7 +9,7 @@ exports.run = async (client, message, args) => {
     cooldown.add(message.author.id);
     setTimeout(() => {
         cooldown.delete(message.author.id);
-    }, 1000);
+    }, 30000);
 var snipes = JSON.parse(fs.readFileSync("./snipe.json", "utf8")); // file containing snipes
 let chn = `${message.channel.id}`;
 var snipechannel = snipes[chn]; // to call an specific deleted message I guess
@@ -21,9 +22,7 @@ if (snipechannel[0] === "No snipes") {
   
   if (snipechannel[0]) embed.setDescription(`\"${snipechannel[0]}\"`);
   
-  if (!snipechannel[2] === null) {
-    embed.setImage(snipechannel[2]);
-  }
+  if (snipechannel[2]) embed.setImage(snipechannel[2]);
   
   await message.channel.send(embed);
   
