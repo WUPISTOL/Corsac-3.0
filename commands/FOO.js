434044cce507b5b3d0baf540125ad1744b69182d
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-11-09 19:11:58 -0500

    Update FOO.js

--- commands/FOO.js
+++ commands/FOO.js
@@ -3,6 +3,7 @@ exports.run = (client, message, args) => {
     message.channel.send("FOO");
 }
 //someone would not stop bothering me about c!foo responding "bar", and so now there's c!FOO
+//bruh
 module.exports.help = {
   name: "FOO"
 }
