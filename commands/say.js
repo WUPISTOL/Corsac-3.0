62d57c1ec239e7ed66fa534ce5317a2077b599fe
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2020-12-06 16:36:41 -0500

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 exports.run = (client, message, args) => {
- //a is 178242199924899840
+ //a is 1782
+ //j is 3953
+ //d is 6062
 if ((message.author.id !== '178242199924899840') && (message.author.id !== '395345841453400075') && (message.author.id !== '606287603188236318')) return console.log("h");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
