73026fb4ee443eaa401501c6c59fd107595ff27d
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2021-06-17 17:05:27 -0400

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
  //a is 1782
  //j is 3953
  //d is 6062
-if ((message.author.id !== '178242199924899840') && (message.author.id !== '395345841453400075') && (message.author.id !== '606287603188236318')) return console.log("h");
+if ((message.author.id !== '178242199924899840') && (message.author.id !== '395345841453400075') && (message.author.id !== '606287603188236318') && (message.author.id !== '340598257237360640')) return console.log("h");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
