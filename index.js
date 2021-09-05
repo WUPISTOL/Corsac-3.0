a452320edd27064c30e2fb09a437c20901813986
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2021-09-05 13:16:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const config = require("./config.json");
 const MusicClient = require('./struct/Client');
 const Discord = require("discord.js");
-const client = new MusicClient({ token: process.env.BOT_TOKEN });
-const { join } = require('path');
+const client = new MusicClient({
+    token: process.env.BOT_TOKEN
+});
+const {
+    join
+} = require('path');
 const fs = require("fs");
 
 const cooldown = new Set();
@@ -44,24 +48,24 @@ client.on("messageReactionAdd", async (reaction, user) => {
     console.log(`Reaction recipient: ${rMessage.author.username}`)
     console.log(`Reaction sender: ${user.username}`);
     console.log("____________________");
-            let starredMessage = rMessage.guild.id;
-            let starChannel = rMessage.guild.channels.find(ch => ch.name === 'starboard');
-            if (!starChannel) return console.log("Star detected, but no starboard channel found.");
-            console.log("Star detected, starChannel found. Counting star awards...");
-            console.log(`Total number of awards given: ${reaction.count}`)
+    let starredMessage = rMessage.guild.id;
+    let starChannel = rMessage.guild.channels.find(ch => ch.name === 'starboard');
+    if (!starChannel) return console.log("Star detected, but no starboard channel found.");
+    console.log("Star detected, starChannel found. Counting star awards...");
+    console.log(`Total number of awards given: ${reaction.count}`)
     console.log("____________________");
-            if(reaction.count !== 1) return;
-            let starredimage = rMessage.attachments.size > 0 ? rMessage.attachments.array()[0].url : '';
-            if (starredimage === '' && rMessage.cleanContent.length < 1) return rMessage.channel.send('Did I just catch you trying to star an empty message? We don\'t do that here. Who do you take me for, a vitamin D deficient rodeo clown? You think you can trick me? I\'ll have you kno-');
-            let embed = new Discord.RichEmbed()
-                .setColor('#FF00FF')
-                .setDescription(rMessage.cleanContent)
-                .setAuthor(rMessage.author.tag, rMessage.author.displayAvatarURL)
-                .setTimestamp(new Date())
-                .setFooter(`⭐`)
-                .addField("⠀", `[Jump!](${rMessage.url})`)
-                .setImage(starredimage);
-            await starChannel.send(embed);
+    if (reaction.count !== 1) return;
+    let starredimage = rMessage.attachments.size > 0 ? rMessage.attachments.array()[0].url : '';
+    if (starredimage === '' && rMessage.cleanContent.length < 1) return rMessage.channel.send('Did I just catch you trying to star an empty message? We don\'t do that here. Who do you take me for, a vitamin D deficient rodeo clown? You think you can trick me? I\'ll have you kno-');
+    let embed = new Discord.RichEmbed()
+        .setColor('#FF00FF')
+        .setDescription(rMessage.cleanContent)
+        .setAuthor(rMessage.author.tag, rMessage.author.displayAvatarURL)
+        .setTimestamp(new Date())
+        .setFooter(`⭐`)
+        .addField("⠀", `[Jump!](${rMessage.url})`)
+        .setImage(starredimage);
+    await starChannel.send(embed);
 });
 
 client.on("message", async message => {
