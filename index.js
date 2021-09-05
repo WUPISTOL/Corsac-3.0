7516827e1d62b3fb3311c820750793be6b82d86b
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2021-09-05 13:06:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const config = require("./config.json");
 const MusicClient = require('./struct/Client');
 const Discord = require("discord.js");
-const client = new MusicClient({ token: process.env.DISCORD_TOKEN, prefix: process.env.DISCORD_PREFIX });
+const client = new MusicClient({ token: process.env.BOT_TOKEN });
 const { join } = require('path');
 const fs = require("fs");
 
