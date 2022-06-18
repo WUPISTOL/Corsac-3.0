90e1295601c47a3503dfc25887006a66ead56983
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2022-06-18 00:14:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const Discord = require("discord.js");
 var ffmpeg = require('ffmpeg');
 const { OpusEncoder } = require('@discordjs/opus');
 
-// Create the encoder.
-// Specify 48kHz sampling rate and 2 channel size.
-const encoder = new OpusEncoder(48000, 2);
-
-// Encode and decode.
-const encoded = encoder.encode(buffer);
-const decoded = encoder.decode(encoded);
+// Specify 48kHz sampling rate
 const client = new Discord.Client({
     intents: ["GUILDS",
         "GUILD_MESSAGES",
