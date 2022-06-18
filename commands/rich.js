aae16a5f250fe79faed9d8ce15d5b59aa06f8b0a
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2022-06-17 22:36:35 -0400

    Update rich.js

--- commands/rich.js
+++ commands/rich.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
 if ((message.author.id !== '178242199924899840') && (message.author.id !== '395345841453400075') && (message.author.id !== '606287603188236318') && (message.author.id !== '340598257237360640')) return console.log("h");
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
-    let embed = new Discord.RichEmbed ()
+    let embed = new Discord.MessageEmbed ()
         .setColor('#0964f7')
         .setDescription(sayMessage)
         message.channel.send(embed);
