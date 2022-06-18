eb9dfe4789d4c1f37e5af62b6b2cedf89e88cb69
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2022-06-17 23:24:02 -0400

    Update p.js

--- commands/p.js
+++ commands/p.js
@@ -5,15 +5,13 @@ if (!message.member.hasPermission("MANAGE_MESSAGES") && (message.author.id !== '
     const messageString = message.content.split(' ');
     const Num = messageString.slice(1).join(' ');
     if(!isNaN(Num)) {
-    if(!Num || Num < 2 || Num > 100)
-    return message.reply("Please choose a number between 2 and 100 to purge.");
-    
-    const fetched = await message.channel.fetchMessages({limit: Num});
-    message.delete().catch(O_o=>{}); 
-    message.channel.bulkDelete(fetched)
-    .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    if(!Num || Num < 2 || Num > 99)  return message.reply("please choose a number between 2 and 99 to purge.");
+    message.channel.bulkDelete(`${Num}`).catch(err => {
+              message.channel.send('Due to Discord Limitations :moyai:, I cannot delete messages older than 14 days') 
+    });
     } else return;
 }
+
 
 module.exports.help = {
   name: "p"
