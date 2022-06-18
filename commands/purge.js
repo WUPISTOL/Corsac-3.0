360ad281e575e1f3cbcf3d70677d9b5d8db204a7
WUPISTOL <48869989+WUPISTOL@users.noreply.github.com>
2022-06-17 23:18:10 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -5,14 +5,9 @@ if (!message.member.hasPermission("MANAGE_MESSAGES") && (message.author.id !== '
     const messageString = message.content.split(' ');
     const Num = messageString.slice(1).join(' ');
     if(!isNaN(Num)) {
-    if(!Num || Num < 2 || Num > 100)  return message.reply("please choose a number between 2 and 99 to purge.");
-    if(Num > 99) {
-        var Max = 100;
-        message.channel.bulkDelete(Max).catch(err => {
-        message.channel.send('Due to Discord Limitations :moyai:, I cannot delete messages older than 14 days') 
-    });
-    } else {
-    message.channel.bulkDelete((Num + 1)).catch(err => {
+    if(!Num || Num < 2 || Num > 99)  return message.reply("please choose a number between 2 and 99 to purge.")
+    var Max = (Num + 1);
+    message.channel.bulkDelete(Max).catch(err => {
               message.channel.send('Due to Discord Limitations :moyai:, I cannot delete messages older than 14 days') 
     });
     } 
